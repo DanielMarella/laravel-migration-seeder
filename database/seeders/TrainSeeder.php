348b76216290703f0f27dba557aca1328a28e143
DanielMarella <marella.daniel96@gmail.com>
2023-07-21 17:11:39 +0200

    create a new  element  to add on my table

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
 
 class TrainSeeder extends Seeder
 {
@@ -14,6 +15,37 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $trains = [
+            [
+
+            'azienda' => 'Trenitalia',
+            'stazione_di_partenza' => 'Padova',
+            'stazione_di_arrivo' => 'Venezia',
+            'orario_di_partenza' => '15.00',
+            'orario_di_arrivo' => '16.00',
+            'codice_treno' => 12345,
+            'numero_carrozze' => 20,
+            'in_orario' => true,
+            'cancellato' => false,
+            ],
+
+        ];
+
+        foreach ($trains as $train) {
+            $newTrain = new Train();
+            $newTrain->azienda = $train['azienda'];
+            $newTrain->stazione_di_partenza = $train['stazione_di_partenza'];
+            $newTrain->stazione_di_arrivo = $train['stazione_di_arrivo'];
+            $newTrain->orario_di_partenza = $train['orario_di_partenza'];
+            $newTrain->orario_di_arrivo = $train['orario_di_arrivo'];
+            $newTrain->codice_treno = $train['codice_treno'];
+            $newTrain->numero_carrozze = $train['numero_carrozze'];
+            $newTrain->in_orario = $train['in_orario'];
+            $newTrain->cancellato = $train['cancellato'];
+            $newTrain->save();
+
+
+
+        }
     }
 }
