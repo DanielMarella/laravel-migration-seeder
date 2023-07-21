dc9b364dc9af7c0c6571b4ab1074fed2e2a57cd7
DanielMarella <marella.daniel96@gmail.com>
2023-07-21 17:43:06 +0200

    print on page all my train

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,17 @@
 
 <ul>
     @foreach ($trains as $train)
-        <li>
-            {{$train -> azienda }}
-            {{$train -> stazione_di_partenza }}
-            {{$train -> azienda }}
-            {{$train -> azienda }}
-            {{$train -> azienda }}
+        <li class="mb-3">
+            Azienda: <strong>{{$train -> azienda }}</strong> <br>
+            Stazione di partenza: <strong>{{$train -> stazione_di_partenza }}</strong> <br>
+            Stazione di arrivo:<strong> {{$train -> stazione_di_arrivo }}</strong><br>
+            Orario di partenza:<strong> {{$train -> orario_di_partenza }}</strong><br>
+            Orario di arrivo: <strong>{{$train -> orario_di_arrivo }}</strong><br>
+            Codice treno: <strong>{{$train -> codice_treno }}</strong><br>
+            Numero carrozze: <strong>{{$train -> numero_carrozze }}</strong><br>
+            In orario: <strong>{{$train -> in_orario }}</strong><br>
+            Cancellato:<strong> {{$train -> cancellato }}</strong><br>
+
 
 
         </li>
