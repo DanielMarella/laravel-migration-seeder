22ce2ff9a0053eb83df5d43f571449630ccb4f7d
DanielMarella <marella.daniel96@gmail.com>
2023-07-21 16:39:29 +0200

    create a new model

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models;
 class TrainController extends Controller
 {
     public function index(){
+
+
         return view('welcome');
     }
 }
